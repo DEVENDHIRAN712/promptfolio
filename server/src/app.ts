ec76c3f1525c32047680ebb4f32deb5cee77edaf
DEVENDHIRAN712 <devendhiranofficial@gmail.com>
2026-09-06 13:14:30 +0530

    Fix TypeScript NodeNext build for Render

--- server/src/app.ts
+++ server/src/app.ts
@@ -48,7 +48,7 @@ app.get('/robots.txt', (_req: Request, res: Response) => {
 
 app.get('/sitemap.xml', async (_req: Request, res: Response) => {
   try {
-    const { Profile } = await import('./models/Profile');
+    const { Profile } = await import('./models/Profile.js');
     const profiles = await Profile.find({ isPublished: true });
     let xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
     xml += `  <url>\n    <loc>http://localhost:5173/</loc>\n    <changefreq>daily</changefreq>\n    <priority>1.0</priority>\n  </url>\n`;
